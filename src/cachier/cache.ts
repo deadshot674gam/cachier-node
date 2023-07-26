5ac30259a3aa2e917f8efc6f7a71c8ca55ed8701
Divyanshu Tiwari <sunnyt0225@gmail.com>
2023-07-26 20:51:41 +0000

    Created Cache, Cachier, HashCachier

---
+++ src/cachier/cache.ts
@@ -0,0 +1,26 @@
+
+
+export class Cache<T> {
+    object: T | undefined
+    ttl: number | undefined
+    createTime: number
+
+    constructor(object: T, ttl?: number) {
+        this.object = object
+        this.ttl = ttl
+        this.createTime = Date.now()
+        if(ttl !== undefined){
+            setTimeout(this.modifyCache, ttl, undefined)
+        }
+    }
+
+    modifyCache(object: T | undefined) {
+        this.object = object
+    }
+
+    modifyTTL(ttl: number) {
+        this.ttl = ttl
+    }
+
+
+}
